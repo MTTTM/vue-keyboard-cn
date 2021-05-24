144ee795ffbd85f5bde48426fd4d87b9a6cc172f
boolean <y133@163.com>
2021-05-24 23:37:58 +0800

    添加测试watch

---
+++ test/counter.test.js
@@ -0,0 +1,16 @@
+import { mount } from '@vue/test-utils'
+import Counter from '../testComponents/components/counter'
+
+describe('Counter', () => {
+  // 现在挂载组件，你便得到了这个包裹器
+  const wrapper = mount(Counter)
+
+  it('renders the correct markup', () => {
+    expect(wrapper.html()).toContain('<span class="count">0</span>')
+  })
+
+  // 也便于检查已存在的元素
+  it('has a button', () => {
+    expect(wrapper.contains('button')).toBe(true)
+  })
+})
