df97ed97c0f78bff3dffd841fcbd5c9e87feebcd
boolean <y133@163.com>
2021-06-05 20:26:52 +0800

    单元测试覆盖报告生成成功！！

--- karma.conf.js
+++ karma.conf.js
@@ -6,12 +6,20 @@ module.exports = function(config) {
 
     files: [
       'test/**/*.spec.js',
-      'src/dev/**/*.js'
+      'src/dev/**/*.js',
+      'src/dev/**/*.vue'
     ],
-
+    /**
+     * 我们用到了babel-plugin-istanbul
+     * 它要求不要再这里加coverage，因为它已经集成了，
+     * 如果再添加corverage，会修改原来的打桩，
+     * 导致打桩代码无法找到原来的对象
+     * https://github.com/istanbuljs/babel-plugin-istanbul
+     */
     preprocessors: {
       '**/*.spec.js': ['webpack', 'sourcemap'],
-      'src/dev/**/*.js':['webpack', 'sourcemap','coverage'],
+      'src/dev/**/*.js':['webpack', 'sourcemap',],
+      'src/dev/**/*.vue':['webpack', 'sourcemap'],
     },
     webpack: webpackConfig,
 
@@ -22,5 +30,6 @@ module.exports = function(config) {
     },
 
     browsers: ['Chrome'],
+    singleRun:true
   })
 }
