acf6366812eb8702a12a93e92d60fc4d3db09852
boolean <y133@163.com>
2021-06-12 14:49:04 +0800

    pages

--- webpack.demo.js
+++ webpack.demo.js
@@ -4,7 +4,7 @@ var HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
   entry: './src/main.js',
   output: {
-    path: path.resolve(__dirname, './demoDist'),
+    path: path.resolve(__dirname, './docs'),
     publicPath: '/',
     filename: 'build.js',
     filename: 'build.[chunkHash:8].js'
