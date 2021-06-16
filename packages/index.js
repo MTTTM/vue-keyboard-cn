144f86bd2cd77fbf941e41a648c822d6d395a087
boolean <y133@163.com>
2021-06-16 16:28:37 +0800

    npm publish --tag=beta

--- packages/index.js
+++ packages/index.js
@@ -1,7 +1,7 @@
 /**
  * npm包入口文件
  */
-import KbInputComponent from '../src/dev/input.vue';
-import KbBoardComponent from "../src/dev/index.vue";
-export const KbInput= KbInputComponent;
-export const KbBoard= KbBoardComponent;
+import KyInputComponent from '../src/dev/input.vue';
+import KyBoardComponent from "../src/dev/index.vue";
+export const KyInput= KyInputComponent;
+export const KyBoard= KyBoardComponent;
