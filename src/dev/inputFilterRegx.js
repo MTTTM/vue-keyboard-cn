f2b3a2bcaa74c809a04f5dc0b08457c8532a87b1
boolean <y133@163.com>
2021-07-03 16:49:57 +0800

    格式优化

--- src/dev/inputFilterRegx.js
+++ src/dev/inputFilterRegx.js
@@ -1,36 +1,34 @@
-export default{
-   "float":(text,decimal=2)=>{
-    let reg = /^[0-9]+.?[0-9]*$/;
-   let t=String(text);
-   if(!isNaN(Number(decimal))&&decimal>0){
-      let decimalNumber=t.split(".")[1];
-      console.log("decimalNumber.length",decimalNumber,"decimal",decimal)
-      if(decimalNumber&&decimalNumber.length>decimal){
-        
+export default {
+   "float": (text, decimal = 2) => {
+      let reg = /^[0-9]+.?[0-9]*$/;
+      let t = String(text);
+      if (!isNaN(Number(decimal)) && decimal > 0) {
+         let decimalNumber = t.split(".")[1];
+         console.log("decimalNumber.length", decimalNumber, "decimal", decimal)
+         if (decimalNumber && decimalNumber.length > decimal) {
+
+            return false;
+         }
+         else {
+            return reg.test(t);
+         }
+      }
+      else {
          return false;
       }
-      else{
-         return reg.test(t);
-      }
-   }
-   else{
-      return false;
-   }
-    
-    
    },
-   "int":(text)=>{
+   "int": (text) => {
       return /^[0-9]*$/.test(text);
    },
-   "cn":(text)=>{
-     //支持中文和中文符号"·"
-    let reg = new RegExp(
-      "^([\u4E00-\u9FFF]|·)+$",
-      "g"
-    );
-    return reg.test(text);
+   "cn": (text) => {
+      //支持中文和中文符号"·"
+      let reg = new RegExp(
+         "^([\u4E00-\u9FFF]|·)+$",
+         "g"
+      );
+      return reg.test(text);
    },
-   "en":(text)=>{
+   "en": (text) => {
       return /^[A-Za-z]+$/.test(text)
    }
 }
